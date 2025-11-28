d4f80a207b75a555426a3d022e64e4649d919264
daraqmarosli <90606494+Babylegend06@users.noreply.github.com>
2025-11-28 22:25:19 +0800

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// Optional: For ESP32 realtime updates and monitoring
-import { db, ref, onValue } from './firebase.js';
-
-// Listen machine statuses
-onValue(ref(db, 'machines/'), snapshot => {
-  const machines = snapshot.val();
-  console.log("Current machine status:", machines);
-});
